18fb01c0d7a22369c75b4438ea56a34b4de98675
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-07-13 21:42:45 +0430

    code rafactored. requirements file updated.

--- chess-ai.py
+++ chess-ai.py
@@ -6,8 +6,8 @@ game = chess.Board()
 
 search_depth = int(input("Enter AI search depth: "))
 
-
 positionCount = None
+
 
 def minmaxRoot(depth, game: chess.Board, isMaximisingPlayer):
     newGameMoves = game.legal_moves
@@ -18,60 +18,54 @@ def minmaxRoot(depth, game: chess.Board, isMaximisingPlayer):
         game.push(newGameMove)
         value = minmax(depth - 1, game, -10000, 10000, not isMaximisingPlayer)
         game.pop()
-        if (value >= bestMove):
+        if value >= bestMove:
             bestMove = value
             bestMoveFound = newGameMove
-    
+
     return bestMoveFound
-
-
 
 
 def minmax(depth, game: chess.Board, alpha, beta, isMaximisingPlayer):
     global positionCount
     positionCount += 1
 
-    if (depth == 0): 
+    if depth == 0:
         return -evaluateBoard(game)
 
     newGameMoves = game.legal_moves
 
-    if (isMaximisingPlayer):
+    if isMaximisingPlayer:
         bestMove = -9999
         for newGameMove in newGameMoves:
             game.push(newGameMove)
             bestMove = max(bestMove, minmax(depth - 1, game, alpha, beta, not isMaximisingPlayer))
             game.pop()
             alpha = max(alpha, bestMove)
-            if (beta <= alpha):
+            if beta <= alpha:
                 return bestMove
-        
+
         return bestMove
     else:
         bestMove = 9999
         for newGameMove in newGameMoves:
             game.push(newGameMove)
-            bestMove = min(bestMove, minmax(depth -  1, game, alpha, beta, not isMaximisingPlayer))
+            bestMove = min(bestMove, minmax(depth - 1, game, alpha, beta, not isMaximisingPlayer))
             game.pop()
             beta = min(beta, bestMove)
-            if (beta <= alpha):
+            if beta <= alpha:
                 return bestMove
-        
+
         return bestMove
-
-
 
 
 def evaluateBoard(game: chess.Board):
     totalEvaluation = 0
     for i in range(8):
         for j in range(8):
-            square = chess.Square(i*8 + j)
+            square = chess.Square(i * 8 + j)
             totalEvaluation += getPieceValue(game.piece_at(square), i, j)
 
     return totalEvaluation
-
-
 
 
 def reverseList(list: list):
@@ -79,116 +73,105 @@ def reverseList(list: list):
     tsil.reverse()
     return tsil
 
+
 pawnEvalWhite = [
-    [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
-    [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
-    [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
-    [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
-    [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
-    [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
-    [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
-    [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+    [1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0],
+    [0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5],
+    [0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+    [0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5],
+    [0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 ]
 
 pawnEvalBlack = reverseList(pawnEvalWhite)
 
-
-
 knightEval = [
     [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
-    [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
-    [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
-    [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
-    [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
-    [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
-    [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+    [-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0],
+    [-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0],
+    [-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0],
+    [-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0],
+    [-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0],
+    [-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0],
     [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
 ]
 
-
-
 bishopEvalWhite = [
-    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
-    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
-    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
-    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
-    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
-    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
-    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
-    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+    [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+    [-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0],
+    [-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0],
+    [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0],
+    [-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0],
+    [-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0],
+    [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
 ]
 
 bishopEvalBlack = reverseList(bishopEvalWhite)
 
-
-
 rookEvalWhite = [
-    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
-    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
-    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
-    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
-    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
-    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
-    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
-    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0]
 ]
 
 rookEvalBlack = reverseList(rookEvalWhite)
 
-
-
 evalQueen = [
-    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
-    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
-    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
-    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
-    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
-    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
-    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
-    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+    [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+    [-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+    [-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+    [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+    [-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+    [-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0],
+    [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
 ]
-
-
 
 kingEvalWhite = [
 
-    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
-    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
-    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
-    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
-    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
-    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
-    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
-    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0],
+    [2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0]
 ]
 
 kingEvalBlack = reverseList(kingEvalWhite)
 
 
-
-
 def getPieceValue(piece: chess.Piece, x, y):
-    if (piece == None):
+    if piece is None:
         return 0
-    
+
     def getAbsoluteValue(piece: chess.Piece, isWhite: bool, x, y):
         piece_symbol = piece.symbol().lower()
-        if (piece_symbol == 'p'):
+        if piece_symbol == 'p':
             return 10 + (pawnEvalWhite[y][x] if isWhite else pawnEvalBlack[y][x])
-        
-        elif (piece_symbol == 'r'):
+
+        elif piece_symbol == 'r':
             return 50 + (rookEvalWhite[y][x] if isWhite else rookEvalBlack[y][x])
-        
-        elif (piece_symbol == 'n'):
+
+        elif piece_symbol == 'n':
             return 30 + knightEval[y][x]
-        
-        elif (piece_symbol == 'b'):
+
+        elif piece_symbol == 'b':
             return 30 + (bishopEvalWhite[y][x] if isWhite else bishopEvalBlack[y][x])
-        
-        elif (piece_symbol == 'q'):
+
+        elif piece_symbol == 'q':
             return 90 + evalQueen[y][x]
-        
-        elif (piece_symbol == 'k'):
+
+        elif piece_symbol == 'k':
             return 900 + (kingEvalWhite[y][x] if isWhite else kingEvalBlack[y][x])
 
         else:
@@ -210,7 +193,7 @@ def getBestMove(game: chess.Board):
     if game.is_game_over():
         print("GAME OVER")
         return None
-    
+
     global positionCount
     positionCount = 0
 
@@ -220,8 +203,6 @@ def getBestMove(game: chess.Board):
     return bestMove
 
 
-
-
 pygame.init()
 window = pygame.display.set_mode((390, 390))
 
@@ -229,34 +210,36 @@ window = pygame.display.set_mode((390, 390))
 def draw_board():
     svg2png(bytestring=game._repr_svg_(), write_to='board.png')
     img = pygame.image.load('board.png')
-    window.blit(img, (0,0))
+    window.blit(img, (0, 0))
     pygame.display.update()
+
 
 draw_board()
 
 
 def find_piece_pos(x, y):
-
     x_sym = chr(ord('a') + int(x / 45)) if x > 0 else 'a'
     y_sym = 1 + int(y / 45) if y > 0 else 1
 
     if x_sym == 'i':
         x_sym = chr(ord(x_sym) - 1)
-    
+
     if y_sym == 9:
         y_sym -= 1
-    
+
     return x_sym + str(y_sym)
-    
+
 
 is_selected = False
 selected_piece = None
+
+
 def color_selection(x, y):
     rect = pygame.Surface((45, 45))
     rect.set_alpha(32)
     rect.fill((0, 255, 0))
-    x_board = 15 + 45*int((x - 15)/45)
-    y_board = 15 + 45*int((y - 15)/45)
+    x_board = 15 + 45 * int((x - 15) / 45)
+    y_board = 15 + 45 * int((y - 15) / 45)
     window.blit(rect, (x_board, y_board))
     pygame.display.update()
 
@@ -264,10 +247,10 @@ def color_selection(x, y):
 while not game.is_game_over():
     pygame.display.update()
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             exit(0)
-        
-        elif event.type==pygame.MOUSEBUTTONUP:
+
+        elif event.type == pygame.MOUSEBUTTONUP:
 
             x, y = pygame.mouse.get_pos()
             piece_pos = find_piece_pos(x - 15, 390 - y - 15)
@@ -287,11 +270,11 @@ while not game.is_game_over():
 
                     makeBestMove()
                     draw_board()
-                
+
                 else:
                     is_selected = False
                     draw_board()
-            
+
             else:
                 for move in game.legal_moves:
                     if move.uci().startswith(piece_pos):
@@ -299,5 +282,3 @@ while not game.is_game_over():
                         is_selected = True
                         selected_piece = piece_pos
                         color_selection(x, y)
-
-
